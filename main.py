f0b4a175164cee7dd616692bc6bf84da93e1fa2b
electrobullet <dmitry.pigasin@yandex.ru>
2022-06-03 00:47:17 +0300

    Implement `get_image` function

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import io
 import os
 
+import cv2 as cv
+import numpy as np
 from aiogram import Bot, Dispatcher, executor, types
 
 bot = Bot(os.environ.get('AGE_RECOGNITION_BOT_TOKEN'))
@@ -7,7 +10,7 @@ dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
+async def info(message: types.Message):
     await message.reply("Hi!\nI'm AgeRecognitionBot!\nPowered by aiogram and OpenVINO.")
 
 
@@ -16,9 +19,18 @@ async def echo(message: types.Message):
     await message.answer('Send me a photo with a face.')
 
 
+async def get_image(photo: types.PhotoSize):
+    image_bytes = io.BytesIO()
+    await photo.download(destination_file=image_bytes)
+    image = np.array(bytearray(image_bytes.getvalue()), np.uint8)
+    image = cv.imdecode(image, cv.IMREAD_COLOR)
+    image_bytes.close()
+    return image
+
+
 @dp.message_handler(content_types=['photo'])
-async def handle_docs_photo(message: types.Message):
-    await message.photo[-1].download(destination_file='test.png')
+async def handle_photo(message: types.Message):
+    cv.imwrite('image.png', await get_image(message.photo[-1]))
 
 
 if __name__ == '__main__':
